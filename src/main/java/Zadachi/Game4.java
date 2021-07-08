4b2564e23aa7391e27e936829df096270e797dd2
34ka <kykyshechka34ka@gmail.com>
2021-07-08 22:27:56 +0600

    Алгоритм вычисляет 3-х значное число в зависимости от входного однозначного числа

---
+++ src/main/java/Zadachi/Game4.java
@@ -0,0 +1,37 @@
+package Zadachi;
+
+/*
+https://acmp.ru/index.asp?main=tasks&str=%20&page=0&id_type=13
+ЗАДАЧА №4
+игра
+ */
+
+public class Game4 {
+    public static void main(String[] args) {
+
+        int a = 9;
+
+        int b = 9;
+        int c = 9 - a;
+
+        if(a == 1) {
+            System.out.println(a + "" + b + "" + c);
+        } else if(a == 2) {
+            System.out.println(a + "" + b + "" + c);
+        } else if(a == 3) {
+            System.out.println(a + "" + b + "" + c);
+        } else if(a == 4) {
+            System.out.println(a + "" + b + "" + c);
+        } else if(a == 5) {
+            System.out.println(a + "" + b + "" + c);
+        } else if(a == 6) {
+            System.out.println(a + "" + b + "" + c);
+        } else if(a == 7) {
+            System.out.println(a + "" + b + "" + c);
+        } else if(a == 8) {
+            System.out.println(a + "" + b + "" + c);
+        } else if(a == 9) {
+            System.out.println(a + "" + b + "" + c);
+        }
+    }
+}
