5bfc590d84dd26048b32034cff3d1504768eece9
34ka <kykyshechka34ka@gmail.com>
2021-10-13 00:15:59 +0600

    Add equals and hashCode. Add methods removeFigure, clearFigures

--- src/main/java/zadachi/shape/Box.java
+++ src/main/java/zadachi/shape/Box.java
@@ -2,19 +2,22 @@ package zadachi.shape;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Box extends Shape {
     private double capacity;
     private double volume;
-    private List<Shape> figures = new ArrayList<>();//Указал, что элементами ArrayList могут быть только элементы типа данных Shape и вмещаемость 15 элементов
+    private List<Shape> figures = new ArrayList<>(5);//Указал, что элементами ArrayList могут быть только элементы типа данных Shape и вмещаемость 15 элементов
 
+    public Box(double capacity) {
+        this.capacity = capacity;
+        this.volume = capacity;
+    }
 
     @Override
     public double getVolume() {
         return volume;
     }
-
-
 
     @Override
     public String toString() {
@@ -23,11 +26,6 @@ public class Box extends Shape {
             result += d.toString();
         }
         return " This is box with objects = " + result;
-    }
-
-    public Box(double capacity) {
-        this.capacity = capacity;
-        this.volume = capacity;
     }
 
     public boolean add(Shape figure) {// т.к. любой класс это тип данных то, создаём в параметрах ссылочную переменную figure типа данных Shape
@@ -44,14 +42,30 @@ public class Box extends Shape {
         return capacity;
     }
 
+    public void clearFigures() {
+        figures.clear();
+        capacity = volume;
+    }
+
     public List<Shape> getFigures() {
         return figures;
     }
 
-    //Нужно  сделать, чтобы он удалял все фигуры и тут же капасити обновляю до изначального значения.
-    // public List<Shape> getFigures() {
-    //        return figures.clear;
-    //    }
+    public void removeFigure(Shape figure) {
+        figures.remove(figure);
+        capacity = capacity + figure.getVolume();
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Box box = (Box) o;
+        return Double.compare(box.capacity, capacity) == 0 && Double.compare(box.volume, volume) == 0 && Objects.equals(figures, box.figures);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(capacity, volume, figures);
+    }
 }
