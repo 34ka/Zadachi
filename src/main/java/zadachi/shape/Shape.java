7bbcd40b5bac321eefad060cef3fc90ae8420636
34ka <kykyshechka34ka@gmail.com>
2021-10-21 02:52:27 +0600

    Added interface

--- src/main/java/zadachi/shape/Shape.java
+++ src/main/java/zadachi/shape/Shape.java
@@ -1,6 +1,6 @@
 package zadachi.shape;
 
-public abstract class Shape {// экземпляр абстрактного класса создать нельзя
-    public abstract double getVolume();
-    public abstract String toString();
+public interface Shape {
+    double getVolume();
+    String toString();
 }
